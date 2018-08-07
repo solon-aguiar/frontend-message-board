f599c8effaa151174f36a2c199492530776926e4
Solon Aguiar <solon.liam.aguiar@gmail.com>
2018-08-07 16:06:39 -0400

    Create Header empty component

---
+++ src/components/Header/index.js
@@ -0,0 +1,26 @@
+import './styles.css';
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <header className="c1pr40a">
+          <div className="cynqhgr">
+            <h1 className="c17defmp">Message board</h1>
+            {this.props.isLoading && (
+              <span role="progressbar" className="c1g8hd9e c1sg2lsz">
+                <div className="c183ltat" style={{animationDelay: "-320ms", background: "black"}}></div>
+                <div className="c183ltat" style={{animationDelay: "-160ms", background: "black"}}></div>
+                <div className="c183ltat" style={{background: "black"}}></div>
+              </span>
+            )}
+          </div>
+          <p className="cp6shng">A place to post and read messages.</p>
+      </header>
+    )
+  }
+}
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,36 +1,45 @@
 import React, { Component } from 'react';
-//import { hot } from 'react-hot-loader';
 import { connect } from 'react-redux';
-import {listMessages} from '../../actions/MessageActions';
+import * as MessageActions from '../../actions/MessageActions';
+import * as ColorActions from '../../actions/ColorActions';
+import Header from '../../components/Header';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    console.log("props", props);
-    this.props.getMessages();
+  }
+
+  componentDidMount() {
+    this.props.listColors();
+    this.props.listMessages();
   }
 
   render() {
+    console.log("props", this.props);
+    const {colors, messages} = this.props;
     return (
-      <div>
-        My App.
+      <div id="app">
+        My App
+        <Header isLoading={colors.listing || messages.listing || messages.adding || messages.searching} />
       </div>
     );
   }
 };
 
 const mapStateToProps = (state) => {
-   console.log("state", state.messages);
-    return {
-        
-    };
+  return {
+    messages: state.messages.toJS(),
+    colors: state.colors.toJS()
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        getMessages: () => dispatch(listMessages())
-        //fetchData: (url) => dispatch(itemsFetchData(url))
-    };
+  return {
+    listMessages: () => dispatch(MessageActions.listMessages()),
+    listColors: () => dispatch(ColorActions.listColors()),
+    createMessage: (content, color) => dispatch(MessageActions.createMessage(content, color)),
+    searchMessages: (content, color) => dispatch(MessageActions.searchMessages(content, color))
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
