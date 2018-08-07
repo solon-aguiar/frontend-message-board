1b4e1db00351d8ecc13972db3a0ba9794d7f481f
Solon Aguiar <solon.liam.aguiar@gmail.com>
2018-08-07 10:50:27 -0400

    MessageService with create

--- src/services/MessageService/__tests__/MessageServiceTests.js
+++ src/services/MessageService/__tests__/MessageServiceTests.js
@@ -5,19 +5,32 @@ import MessageService from '../../MessageService';
 describe('MessageService', () => {
   const mockResponse = 'my-fake-response';
 
-  describe('getMessages', () => {
+  describe('get', () => {
     it('calls the HttpService with a search query', () => {
       const fetchMock = jest.fn().mockReturnValue(mockResponse);
       const subject = new MessageService(fetchMock);
 
-      const messages = subject.getMessages('my-query-string', 'blue');
+      const messages = subject.get('my-query-string', 'blue');
       expect(messages).toEqual(mockResponse);
       expect(fetchMock.mock.calls.length).toEqual(1);
       expect(fetchMock.mock.calls[0][0]).toEqual(`/messages?_sort=id&_order=desc&q=my-query-string&color=blue`);
     });
   });
 
-  describe('createMessage', () => {
+  describe('create', () => {
+    it('uses the HttpService', () => {
+      const fetchMock = jest.fn().mockReturnValue(mockResponse);
+      const subject = new MessageService(fetchMock);
 
+      const messages = subject.create('content', 'blue');
+      expect(messages).toEqual(mockResponse);
+      expect(fetchMock.mock.calls.length).toEqual(1);
+      expect(fetchMock.mock.calls[0][0]).toEqual(`/messages`);
+      expect(fetchMock.mock.calls[0][1]).toEqual({
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: '{"content":"content","color":"blue"}' 
+      });
+    });
   });
 });
--- src/services/MessageService/index.js
+++ src/services/MessageService/index.js
@@ -8,7 +8,14 @@ export default class MessageService extends HttpService {
     this.path = '/messages';
   }
 
-  getMessages(searchText, color) {
+  get(searchText, color) {
     return super.get(this.path, `_sort=id&_order=desc&q=${searchText}&color=${color}`);
+  }
+
+  create(content, color) {
+    return super.post(this.path, {
+      content,
+      color
+    });
   }
 }
