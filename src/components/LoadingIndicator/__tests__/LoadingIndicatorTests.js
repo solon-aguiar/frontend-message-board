813a950685637f9ba1df988e168907225b15bf40
Solon Aguiar <solon.liam.aguiar@gmail.com>
2018-08-07 22:02:39 -0400

    LoadingIndicatorTests finalized

--- src/components/LoadingIndicator/__tests__/LoadingIndicatorTests.js
+++ src/components/LoadingIndicator/__tests__/LoadingIndicatorTests.js
@@ -1,33 +1,33 @@
 import React from 'react'
-import Enzyme, { mount } from 'enzyme'
+import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16';
 import LoadingIndicator from '../../LoadingIndicator';
 import setUpJsDom from '../../__tests-setup__';
 
-setUpJsDom();
+//setUpJsDom();
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('LoadingIndicator', () => {
-  function setup(props) {
-    const enzymeWrapper = mount(<LoadingIndicator />);
-
-    return {
-      props,
-      enzymeWrapper
-    };
-  }
-
   it('should render self and subcomponents with default props', () => {
-    const { enzymeWrapper } = setup();
+    const enzymeWrapper = shallow(<LoadingIndicator />);
 
     expect(enzymeWrapper.find('span').hasClass('internal-loading-indicator-center')).toBe(true);
-    // expect(enzymeWrapper.find('span').hasClass('internal-loading-indicator-center')).toBe(true)
-
-    // expect(enzymeWrapper.find('h1').text()).toBe('todos')
-
-    // const todoInputProps = enzymeWrapper.find('TodoTextInput').props()
-    // expect(todoInputProps.newTodo).toBe(true)
-    // expect(todoInputProps.placeholder).toEqual('What needs to be done?')
+    expect(enzymeWrapper.find('div').length).toBe(3);
+    expect(enzymeWrapper.find('.blue-dot').length).toBe(3);
+    expect(enzymeWrapper.find('.blue-dot').get(0).props.style).toEqual({animationDelay: '-320ms'});
+    expect(enzymeWrapper.find('.blue-dot').get(1).props.style).toEqual({animationDelay: '-160ms'});
+    expect(enzymeWrapper.find('.blue-dot').get(2).props.style).toBe(undefined);
   });
+
+  it('should render with the specified cssClass and background', () => {
+    const enzymeWrapper = shallow(<LoadingIndicator background cssClass={'companion-loading-indicator'} />);
+
+    expect(enzymeWrapper.find('span').hasClass('companion-loading-indicator')).toBe(true);
+    expect(enzymeWrapper.find('div').length).toBe(3);
+    expect(enzymeWrapper.find('.blue-dot').length).toBe(3);
+    expect(enzymeWrapper.find('.blue-dot').get(0).props.style).toEqual({animationDelay: '-320ms', background: "black"});
+    expect(enzymeWrapper.find('.blue-dot').get(1).props.style).toEqual({animationDelay: '-160ms', background: "black"});
+    expect(enzymeWrapper.find('.blue-dot').get(2).props.style).toEqual({background: "black"});
+  })
 
 });
