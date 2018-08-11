5bd23b0a4d55a602f6f8e4771269dc8c600469fe
Solon Aguiar <solon.liam.aguiar@gmail.com>
2018-08-11 12:00:55 -0400

    Reorganize CreateMessageWidgetTests

--- src/components/CreateMessageWidget/__tests__/CreateMessageWidgetTests.js
+++ src/components/CreateMessageWidget/__tests__/CreateMessageWidgetTests.js
@@ -32,152 +32,164 @@ describe('CreateMessageWidget', () => {
     addMessageMock.mockClear();
   });
 
-  it('shows the subcomponents', () => {
-    const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
+  describe('appearance: ', () => {
+    it('shows the subcomponents', () => {
+      const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
 
-    expect(enzymeWrapper.find(MessageContentInput).exists()).toBe(true);
-    expect(enzymeWrapper.find(MessageColorInput).exists()).toBe(true);
-    expect(enzymeWrapper.find(ClinikoButton).exists()).toBe(true);
+      expect(enzymeWrapper.find(MessageContentInput).exists()).toBe(true);
+      expect(enzymeWrapper.find(MessageColorInput).exists()).toBe(true);
+      expect(enzymeWrapper.find(ClinikoButton).exists()).toBe(true);
+    });
+
+    it('shows all the colors as options', () => {
+      const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
+
+      expect(enzymeWrapper.find(MessageColorInput).prop('options')).toEqual(colors);
+      expect(enzymeWrapper.find(MessageColorInput).prop('label')).toEqual('Color');
+    });
+
+    it('displays the default selected color', () => {
+      const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
+
+      expect(enzymeWrapper.find(MessageColorInput).prop('defaultOption')).toEqual('Choose a color...');
+    });
+
+    it('shows an empty message as message content', () => {
+      const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
+
+      expect(enzymeWrapper.find(MessageContentInput).prop('content')).toEqual('');
+      expect(enzymeWrapper.find(MessageContentInput).prop('label')).toEqual('Message');
+    });
+
+    it('disables the submit button on start', () => {
+      const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
+
+      expect(enzymeWrapper.find(ClinikoButton).prop('disabled')).toEqual(true);
+    });
+
   });
 
-  it('shows the colors as options', () => {
-    const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
+  describe('behavior -', () => {
+    describe('when provided input', () => {
+      it('displays the current message content', () => {
+        const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
+        enzymeWrapper.setState({messageContent: inputMessage});
 
-    expect(enzymeWrapper.find(MessageColorInput).prop('options')).toEqual(colors);
-    expect(enzymeWrapper.find(MessageColorInput).prop('label')).toEqual('Color');
+        expect(enzymeWrapper.find(MessageContentInput).prop('content')).toEqual(inputMessage);
+      });
+
+      it('records the message content', () => {
+        const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} isAddingMessage={true} />);
+        enzymeWrapper.find(MessageContentInput).prop('onChange')({target:{value:inputMessage}});
+
+        expect(enzymeWrapper.state('messageContent')).toEqual(inputMessage);
+      });
+
+      it('shows error message for empty message content', () => {
+        const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
+        expect(enzymeWrapper.find(MessageContentInput).prop('showError')).toEqual(false);
+
+        enzymeWrapper.find(MessageContentInput).prop('onChange')({target:{value:" "}});
+        enzymeWrapper.update();
+
+        expect(enzymeWrapper.find(MessageContentInput).prop('showError')).toEqual(true);
+        expect(enzymeWrapper.state('messageContent')).toEqual(" ");
+      });
+
+      it('removes existing error message for valid inputs', () => {
+        const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
+
+        enzymeWrapper.find(MessageContentInput).prop('onChange')({target:{value:" "}});
+        enzymeWrapper.update();
+        expect(enzymeWrapper.find(MessageContentInput).prop('showError')).toEqual(true);
+
+        enzymeWrapper.find(MessageContentInput).prop('onChange')({target:{value:"a valid message now!"}});
+        enzymeWrapper.update();
+        expect(enzymeWrapper.find(MessageContentInput).prop('showError')).toEqual(false);
+      });
+
+      it('shows error for empty content on blur', () => {
+        const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
+        expect(enzymeWrapper.find(MessageContentInput).prop('showError')).toEqual(false);
+
+        enzymeWrapper.find(MessageContentInput).prop('onBlur')({});
+        enzymeWrapper.update();
+
+        expect(enzymeWrapper.find(MessageContentInput).prop('showError')).toEqual(true);
+      });
+    });
+
+    describe('when provided selection', () => {
+      it('displays the message color', () => {
+        const selectedColor = 'rainbow';
+
+        const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
+        enzymeWrapper.setState({messageColor: selectedColor});
+
+        expect(enzymeWrapper.find(MessageColorInput).prop('selected')).toEqual(selectedColor);
+      });
+
+      it('records the message color', () => {
+        const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
+        enzymeWrapper.find(MessageColorInput).prop('onSelect')(selectedColor);
+
+        expect(enzymeWrapper.state('messageColor')).toEqual(selectedColor);
+      });
+    });
+
+    describe('when adding new messages', () => {
+      it('adds a loading indicator', () => {
+        const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} isAddingMessage={true} />);
+        expect(enzymeWrapper.find(ClinikoButton).prop('showLoadingIndicator')).toEqual(true);
+
+        const anotherEnzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} isAddingMessage={false} />);
+        expect(anotherEnzymeWrapper.find(ClinikoButton).prop('showLoadingIndicator')).toEqual(false);
+        expect(enzymeWrapper.find(MessageContentInput).prop('showLoading')).toEqual(false);
+      });
+    });
+
+    describe('when submitting', () => {
+      it('allows the submission when no color is chosen', () => {
+        const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
+        enzymeWrapper.find(MessageContentInput).prop('onChange')({target:{value:"a valid message now!"}});
+
+        expect(enzymeWrapper.instance().canSubmit()).toEqual(true);
+      });
+
+      it('allows submission for valid data', () => {
+        const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
+        enzymeWrapper.find(MessageContentInput).prop('onChange')({target:{value:inputMessage}});
+        enzymeWrapper.find(MessageColorInput).prop('onSelect')(selectedColor);
+
+        expect(enzymeWrapper.instance().canSubmit()).toEqual(true);
+
+        enzymeWrapper.update();
+        expect(enzymeWrapper.find(ClinikoButton).prop('disabled')).toEqual(false);
+      });
+
+      it('triggers the callback', async () => {
+        const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
+        enzymeWrapper.find(MessageContentInput).prop('onChange')({target:{value:inputMessage}});
+        enzymeWrapper.find(MessageColorInput).prop('onSelect')(selectedColor);
+
+        enzymeWrapper.find(ClinikoButton).prop('onClick')();
+        expect(addMessageMock.mock.calls.length).toBe(1);
+      });
+
+      it('resets the inputs', async () => {
+        const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
+        enzymeWrapper.find(MessageContentInput).prop('onChange')({target:{value:inputMessage}});
+        enzymeWrapper.find(MessageColorInput).prop('onSelect')(selectedColor);
+
+        enzymeWrapper.find(ClinikoButton).prop('onClick')();
+        await addMessageResponse
+
+        expect(enzymeWrapper.instance().canSubmit()).toEqual(false);
+        expect(enzymeWrapper.state()).toEqual({messageContent: '', messageColor: '', hasMessageContentError: false});
+        expect(enzymeWrapper.find(ClinikoButton).prop('disabled')).toEqual(true);
+        expect(enzymeWrapper.find(MessageColorInput).prop('selected')).toEqual('');
+        expect(enzymeWrapper.find(MessageContentInput).prop('content')).toEqual('');
+      });
+    })
   });
-
-  it('shows an empty message as message content', () => {
-    const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
-
-    expect(enzymeWrapper.find(MessageContentInput).prop('content')).toEqual('');
-    expect(enzymeWrapper.find(MessageContentInput).prop('label')).toEqual('Message');
-  });
-
-  it('disables the submit button on start', () => {
-    const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
-
-    expect(enzymeWrapper.find(ClinikoButton).prop('disabled')).toEqual(true);
-  });
-
-  it('shows loading indicator when adding new messages', () => {
-    const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} isAddingMessage={true} />);
-    expect(enzymeWrapper.find(ClinikoButton).prop('showLoadingIndicator')).toEqual(true);
-
-    const anotherEnzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} isAddingMessage={false} />);
-    expect(anotherEnzymeWrapper.find(ClinikoButton).prop('showLoadingIndicator')).toEqual(false);
-    expect(enzymeWrapper.find(MessageContentInput).prop('showLoading')).toEqual(false);
-  });
-
-  it('displays the current message content on the input', () => {
-    const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
-    enzymeWrapper.setState({messageContent: inputMessage});
-
-    expect(enzymeWrapper.find(MessageContentInput).prop('content')).toEqual(inputMessage);
-  });
-
-  it('records the message content on input', () => {
-    const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} isAddingMessage={true} />);
-    enzymeWrapper.find(MessageContentInput).prop('onChange')({target:{value:inputMessage}});
-
-    expect(enzymeWrapper.state('messageContent')).toEqual(inputMessage);
-  });
-
-  it('displays the correct default selected color', () => {
-    const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
-
-    expect(enzymeWrapper.find(MessageColorInput).prop('defaultOption')).toEqual('Choose a color...');
-  });
-
-  it('selects the current message color on the options', () => {
-    const selectedColor = 'rainbow';
-
-    const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
-    enzymeWrapper.setState({messageColor: selectedColor});
-
-    expect(enzymeWrapper.find(MessageColorInput).prop('selected')).toEqual(selectedColor);
-  });
-
-  it('records the message color on user selection', () => {
-    const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
-    enzymeWrapper.find(MessageColorInput).prop('onSelect')(selectedColor);
-
-    expect(enzymeWrapper.state('messageColor')).toEqual(selectedColor);
-  });
-
-  it('shows error message for empty message content', () => {
-    const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
-    expect(enzymeWrapper.find(MessageContentInput).prop('showError')).toEqual(false);
-
-    enzymeWrapper.find(MessageContentInput).prop('onChange')({target:{value:" "}});
-    enzymeWrapper.update();
-
-    expect(enzymeWrapper.find(MessageContentInput).prop('showError')).toEqual(true);
-    expect(enzymeWrapper.state('messageContent')).toEqual(" ");
-  });
-
-  it('clears error message on valid inputs', () => {
-    const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
-
-    enzymeWrapper.find(MessageContentInput).prop('onChange')({target:{value:" "}});
-    enzymeWrapper.update();
-    expect(enzymeWrapper.find(MessageContentInput).prop('showError')).toEqual(true);
-    
-    enzymeWrapper.find(MessageContentInput).prop('onChange')({target:{value:"a valid message now!"}});
-    enzymeWrapper.update();
-    expect(enzymeWrapper.find(MessageContentInput).prop('showError')).toEqual(false);
-  });
-
-  it('allows the submission when no color is chosen', () => {
-    const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
-    enzymeWrapper.find(MessageContentInput).prop('onChange')({target:{value:"a valid message now!"}});
-
-    expect(enzymeWrapper.instance().canSubmit()).toEqual(true);
-  });
-
-  it('allows submission for valid data', () => {
-    const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
-    enzymeWrapper.find(MessageContentInput).prop('onChange')({target:{value:inputMessage}});
-    enzymeWrapper.find(MessageColorInput).prop('onSelect')(selectedColor);
-
-    expect(enzymeWrapper.instance().canSubmit()).toEqual(true);
-
-    enzymeWrapper.update();
-    expect(enzymeWrapper.find(ClinikoButton).prop('disabled')).toEqual(false);
-  });
-
-  it('triggers the callback on submission', async () => {
-    const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
-    enzymeWrapper.find(MessageContentInput).prop('onChange')({target:{value:inputMessage}});
-    enzymeWrapper.find(MessageColorInput).prop('onSelect')(selectedColor);
-
-    enzymeWrapper.find(ClinikoButton).prop('onClick')();
-    expect(addMessageMock.mock.calls.length).toBe(1);
-  });
-
-  it('resets the inputs on submission', async () => {
-    const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
-    enzymeWrapper.find(MessageContentInput).prop('onChange')({target:{value:inputMessage}});
-    enzymeWrapper.find(MessageColorInput).prop('onSelect')(selectedColor);
-
-    enzymeWrapper.find(ClinikoButton).prop('onClick')();
-    await addMessageResponse
-
-    expect(enzymeWrapper.instance().canSubmit()).toEqual(false);
-    expect(enzymeWrapper.state()).toEqual({messageContent: '', messageColor: '', hasMessageContentError: false});
-    expect(enzymeWrapper.find(ClinikoButton).prop('disabled')).toEqual(true);
-    expect(enzymeWrapper.find(MessageColorInput).prop('selected')).toEqual('');
-    expect(enzymeWrapper.find(MessageContentInput).prop('content')).toEqual('');
-  });
-
-  it('shows error for empty content on blur', () => {
-    const enzymeWrapper = shallow(<CreateMessageWidget colors={colors} addMessage={addMessageMock} />);
-    expect(enzymeWrapper.find(MessageContentInput).prop('showError')).toEqual(false);
-
-    enzymeWrapper.find(MessageContentInput).prop('onBlur')({});
-    enzymeWrapper.update();
-
-    expect(enzymeWrapper.find(MessageContentInput).prop('showError')).toEqual(true);
-  });
-
 });
