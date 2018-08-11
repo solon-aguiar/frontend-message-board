37e03faeb654a198f56ad0ca92392e8e05d01cf9
Solon Aguiar <solon.liam.aguiar@gmail.com>
2018-08-11 00:45:18 -0400

    Move Criteria to Search

--- src/components/Search/__tests__/SearchTests.js
+++ src/components/Search/__tests__/SearchTests.js
@@ -3,13 +3,17 @@ import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16';
 
 import Search from '../../Search';
+import MessageContentInput from '../../MessageContentInput';
+import MessageColorInput from '../../MessageColorInput';
 import MessageList from '../../MessageList';
-import Criteria from '../../Search/Criteria';
 import LoadingIndicator from '../../LoadingIndicator';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Search', () => {
+  const searchQuery = 'new message content';
+  const searchColor = 'rainbow';
+
   const colors = [
     {
       id: 1,
@@ -22,7 +26,6 @@ describe('Search', () => {
       name: 'Value 2'
     }
   ];
-
   const messages = [
     {
       id: 1,
@@ -35,26 +38,159 @@ describe('Search', () => {
       color: 'value2'
     }
   ];
-  const onChange = jest.fn();
-  const abortExistingRequestMock = jest.fn();
 
-  it('renders the subcomponents', () => {
-    const enzymeWrapper = shallow(<Search colors={colors} searchMessages={onChange} isSearching={false} messages={messages} abortExistingRequest={abortExistingRequestMock} />);
+  const searchMessagesMock = jest.fn();
+  const abortRequestMock = jest.fn();
 
-    expect(enzymeWrapper.find(Criteria).exists()).toBe(true);
+  afterEach(() => {
+    searchMessagesMock.mockReset();
+    abortRequestMock.mockReset();
+  });
+
+  it('renders with subcomponents', () => {
+    const enzymeWrapper = shallow(<Search colors={colors} searchMessages={searchMessagesMock} />);
+
+    expect(enzymeWrapper.find(MessageContentInput).exists()).toBe(true);
+    expect(enzymeWrapper.find(MessageColorInput).exists()).toBe(true);
+    expect(enzymeWrapper.find(MessageList).exists()).toBe(true);
     expect(enzymeWrapper.find(LoadingIndicator).exists()).toBe(false);
-    expect(enzymeWrapper.find(MessageList).exists()).toBe(true);
-
-    expect(enzymeWrapper.find(MessageList).prop('messages')).toEqual(messages);
-    expect(enzymeWrapper.find(Criteria).prop('colors')).toEqual(colors);
-    expect(enzymeWrapper.find(Criteria).prop('abortExistingRequest')).toEqual(abortExistingRequestMock);
   });
 
-  it('shows LoadingIndicator in case an operation is happening', () => {
-    const enzymeWrapper = shallow(<Search colors={colors} searchMessages={onChange} isSearching messages={messages} />);
+  it('shows header loading indicator when search request is happening', () => {
+    const enzymeWrapper = shallow(<Search colors={colors} searchMessages={searchMessagesMock} isSearching />);
 
-    expect(enzymeWrapper.find(Criteria).exists()).toBe(true);
-    expect(enzymeWrapper.find(LoadingIndicator).exists()).toBe(true);
-    expect(enzymeWrapper.find(MessageList).exists()).toBe(true);
+    expect(enzymeWrapper.find('InfoPanel').prop('isLoading')).toBe(true);
   });
+
+  it('displays the number of messages loaded', () => {
+    const enzymeWrapper = shallow(<Search colors={colors} searchMessages={searchMessagesMock} messages={messages} />);
+
+    expect(enzymeWrapper.find('InfoPanel').prop('nMessages')).toEqual(2);
+  });
+
+  it('shows the colors as options', () => {
+    const enzymeWrapper = shallow(<Search colors={colors} searchMessages={searchMessagesMock} />);
+
+    expect(enzymeWrapper.find(MessageColorInput).prop('options')).toEqual(colors);
+    expect(enzymeWrapper.find(MessageColorInput).prop('label')).toEqual('Filter');
+  });
+
+  it('displays the correct default selected color', () => {
+    const enzymeWrapper = shallow(<Search colors={colors} searchMessages={searchMessagesMock} />);
+
+    expect(enzymeWrapper.find(MessageColorInput).prop('defaultOption')).toEqual('All colors');
+  });
+
+  it('displays the color options with proper styles', () => {
+    const enzymeWrapper = shallow(<Search colors={colors} searchMessages={searchMessagesMock} />);
+
+    expect(enzymeWrapper.find(MessageColorInput).prop('style')).toEqual('search-color');
+  });
+
+  it('shows an empty message as message content', () => {
+    const enzymeWrapper = shallow(<Search colors={colors} searchMessages={searchMessagesMock} />);
+
+    expect(enzymeWrapper.find(MessageContentInput).prop('content')).toEqual('');
+    expect(enzymeWrapper.find(MessageContentInput).prop('label')).toEqual('Search');
+  });
+
+  it('shows loading indicator if searching with text', () => {
+    const enzymeWrapper = shallow(<Search colors={colors} searchMessages={searchMessagesMock} isSearching />);
+    enzymeWrapper.find(MessageContentInput).prop('onChange')({target:{value:searchQuery}});
+    enzymeWrapper.update();
+
+    expect(enzymeWrapper.find(MessageContentInput).prop('showLoading')).toBe(true);
+  });
+
+  it('does not show loading indicator if searching with colors', () => {
+    const enzymeWrapper = shallow(<Search colors={colors} searchMessages={searchMessagesMock} isSearching />);
+    enzymeWrapper.find(MessageColorInput).prop('onSelect')(searchColor);
+    enzymeWrapper.update();
+
+    expect(enzymeWrapper.find(MessageContentInput).prop('showLoading')).toBe(false);
+  });
+
+  it('records the search query on input', () => {
+    const enzymeWrapper = shallow(<Search colors={colors} searchMessages={searchMessagesMock} />);
+    enzymeWrapper.find(MessageContentInput).prop('onChange')({target:{value:searchQuery}});
+
+    expect(enzymeWrapper.state('searchQuery')).toEqual(searchQuery);
+  });
+
+  it('displays the current message content on the input', () => {
+    const enzymeWrapper = shallow(<Search colors={colors} searchMessages={searchMessagesMock} />);
+    enzymeWrapper.setState({searchQuery});
+
+    expect(enzymeWrapper.find(MessageContentInput).prop('content')).toEqual(searchQuery);
+  });
+
+  it('records the color on selection', () => {
+    const enzymeWrapper = shallow(<Search colors={colors} searchMessages={searchMessagesMock} />);
+    enzymeWrapper.find(MessageColorInput).prop('onSelect')(searchColor);
+
+    expect(enzymeWrapper.state('selectedColor')).toEqual(searchColor);
+  });
+
+  it('displays the selected color on the options', () => {
+    const enzymeWrapper = shallow(<Search colors={colors} searchMessages={searchMessagesMock} />);
+    enzymeWrapper.setState({selectedColor: searchColor});
+
+    expect(enzymeWrapper.find(MessageColorInput).prop('selected')).toEqual(searchColor);
+  });
+
+  it('triggers new search on query', () => {
+    const myQuery = 'abc';
+
+    const enzymeWrapper = shallow(<Search colors={colors} searchMessages={searchMessagesMock} isSearching />);
+    enzymeWrapper.setState({searchQuery: myQuery});
+
+    expect(searchMessagesMock.mock.calls.length).toBe(1);
+    expect(searchMessagesMock.mock.calls[0]).toEqual([myQuery, ""]);
+  });
+
+  it('triggers new search on color selection', () => {
+    const enzymeWrapper = shallow(<Search colors={colors} searchMessages={searchMessagesMock} isSearching />);
+    enzymeWrapper.setState({selectedColor: searchColor});
+
+    expect(searchMessagesMock.mock.calls.length).toBe(1);
+    expect(searchMessagesMock.mock.calls[0]).toEqual(["", searchColor]);
+  });
+
+  it('aborts existing request and triggers new request on search criteria change', () => {
+    const enzymeWrapper = shallow(<Search colors={colors} searchMessages={searchMessagesMock} isSearching abortExistingRequest={{abort: abortRequestMock}}/>);
+    enzymeWrapper.setState({searchQuery});
+
+    expect(abortRequestMock.mock.calls.length).toBe(1);
+  });
+
+  it('triggers search on new messages added', () => {
+    const enzymeWrapper = shallow(<Search colors={colors} searchMessages={searchMessagesMock} isSearching isAdding />);
+    enzymeWrapper.setProps({isAdding: false});
+
+    expect(searchMessagesMock.mock.calls.length).toBe(1);
+    expect(searchMessagesMock.mock.calls[0]).toEqual(["", ""]);
+  });
+
+  it('aborts existing request and triggers new request on new messages added', () => {
+    const enzymeWrapper = shallow(<Search colors={colors} searchMessages={searchMessagesMock} isSearching isAdding abortExistingRequest={{abort: abortRequestMock}}/>);
+    enzymeWrapper.setProps({isAdding: false});
+
+    expect(abortRequestMock.mock.calls.length).toBe(1);
+    expect(searchMessagesMock.mock.calls.length).toBe(1);
+    expect(searchMessagesMock.mock.calls[0]).toEqual(["", ""]);
+  });
+
+  it('encodes the URI for the parameters', () => {
+    const searchQuery = 'new message content';
+    const encodedSearchQuery = 'new%20message%20content';
+
+    const colorSelected = '#2795D9';
+    const encodedColor = '%232795D9';
+
+    const enzymeWrapper = shallow(<Search colors={colors} searchMessages={searchMessagesMock} isSearching />);
+    enzymeWrapper.setState({searchQuery: searchQuery, selectedColor:colorSelected});
+
+    expect(searchMessagesMock.mock.calls.length).toBe(1);
+    expect(searchMessagesMock.mock.calls[0]).toEqual([encodedSearchQuery, encodedColor]);
+  });  
 });
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -2,27 +2,112 @@
 
 import './styles.css';
 
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import Criteria from './Criteria';
+import DropdownList from '../DropdownList';
 import LoadingIndicator from '../LoadingIndicator';
+import MessageContentInput from '../MessageContentInput';
+import MessageColorInput from '../MessageColorInput';
 import MessageList from '../MessageList';
 
-export default function Search(props) {
+function InfoPanel(props) {
   return (
-    <section>
-      <header className="header">
-        <div className="info-panel">
-          <h2 className="header-text">Messages</h2>
-          <span className="header-text-number">{props.messages.length}</span>
-          {!!props.isSearching && <LoadingIndicator background={true} cssClass={"companion-loading-indicator"} /> }
-        </div>
-        <Criteria abortExistingRequest={props.abortExistingRequest} colors={props.colors} onChange={props.searchMessages} isSearching={props.isSearching} isAdding={props.isAdding} />
-      </header>
-      <MessageList messages={props.messages} />
-    </section>
+    <div className="info-panel">
+      <h2 className="header-text">Messages</h2>
+      <span className="header-text-number">{props.nMessages}</span>
+      {!!props.isLoading && <LoadingIndicator background={true} cssClass={"companion-loading-indicator"} /> }
+    </div>
   );
+}
+
+export default class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedColor: '',
+      searchQuery: '',
+      triggeredQuerySearch: false
+    };
+
+    this.onColorSelected = this.onColorSelected.bind(this);
+    this.handleSearchQueryChange = this.handleSearchQueryChange.bind(this);
+  }
+
+  handleSearchQueryChange(event) {
+    const content = event.target.value;
+
+    this.setState({
+      searchQuery: content,
+      triggeredQuerySearch: true
+    });
+  }
+
+  onColorSelected(color) {
+    this.setState({
+      selectedColor: color,
+      triggeredQuerySearch: false
+    });
+  }
+
+  createdNewMessages(prevProps) {
+    return !!prevProps.isAdding && !this.props.isAdding
+  }
+
+  hasOutstandingSearchRequest() {
+    return !!this.props.abortExistingRequest;
+  }
+
+  changedSearchParameters(prevProps, prevState) {
+    return prevState.selectedColor != this.state.selectedColor || prevState.searchQuery != this.state.searchQuery
+  }
+
+  abortExistingSearches() {
+    if (this.hasOutstandingSearchRequest()) {
+      this.props.abortExistingRequest.abort();
+    }
+  }
+
+  isSearchQuery() {
+    return this.state.triggeredQuerySearch || !!this.state.searchQuery;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.changedSearchParameters(prevProps, prevState) || this.createdNewMessages(prevProps)) {
+      this.abortExistingSearches();
+      this.props.searchMessages(encodeURIComponent(this.state.searchQuery), encodeURIComponent(this.state.selectedColor));
+    }
+  }
+
+  render() {
+    return (
+      <section>
+        <header className="header">
+          <InfoPanel isLoading={this.props.isSearching} nMessages={!!this.props.messages ? this.props.messages.length : 0} />
+
+          <div className="search-input-container">
+            <MessageContentInput
+              label={"Search"}
+              content={this.state.searchQuery}
+              onChange={this.handleSearchQueryChange}
+              showLoading={this.props.isSearching && this.isSearchQuery()}
+              showError={false}
+            />
+            <MessageColorInput
+              defaultOption={'All colors'}
+              style={"search-color"}
+              label={"Filter"}
+              options={this.props.colors}
+              onSelect={this.onColorSelected}
+              selected={this.state.selectedColor}
+            />
+          </div>
+        </header>
+        <MessageList messages={this.props.messages} />
+      </section>
+    );
+  }
 }
 
 Search.propsTypes = {
@@ -36,8 +121,9 @@ Search.propsTypes = {
     color: PropTypes.string.isRequired,
     id: PropTypes.number.isRequired
   })).isRequired,
-  searchMessages: PropTypes.func.isRequired,
-  abortExistingRequest: PropTypes.func,
+  LoadingIndicator: PropTypes.func.isRequired,
   isSearching: PropTypes.bool.isRequired,
-  isAdding: PropTypes.bool.isRequired
+  isAdding: PropTypes.bool.isRequired,
+  abortExistingRequest: PropTypes.func,
+  isSearching: PropTypes.bool.isRequired
 };
